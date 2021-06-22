f7d1b9986f43dd2e2eb86fff09d594e08458eceb
EDO-MAN <73830965+EDO-MAN@users.noreply.github.com>
2021-06-22 21:44:53 +0900

    Board정의
    이제 시작!!!

---
+++ Socoban/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Socoban
+{
+    public partial class Form1 : Form
+    {
+        int board_W;
+        int board_H;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            board_W = GameRule.Board_W;
+            board_H = GameRule.Board_H;
+
+            SetClientSizeCore(board_W, board_H);
+        }
+    }
+}
---
+++ Socoban/GameRule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Socoban
+{
+    public class GameRule
+    {
+        public const int Board_W = 1000;
+        public const int Board_H = 700;
+    }
+}
